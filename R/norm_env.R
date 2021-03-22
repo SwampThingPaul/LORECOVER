3bf6c4be1adfddd7a1ca1f1de630750a1b8b21d1
Paul Julian <36565183+SwampThingPaul@users.noreply.github.com>
2021-03-22 08:37:41 -0400

    edits but now note

--- R/norm_env.R
+++ R/norm_env.R
@@ -1,10 +1,10 @@
 
 #' Normal Lake Stage Envelope Score
 #'
-#' @param data see details
+#' @param stg.data see details
 #'
 #' @details
-#' The input `data` is a `data.frame` with columns:
+#' The input `stg.data` is a `data.frame` with columns:
 #' \itemize{
 #' \item{`Date` (as a `POSIXct` or `Date` variable)}
 #' \item{`Data.Value` as stage elevation data in feet (NGVD29)}
@@ -24,40 +24,40 @@
 #'
 #' norm_env(dat)
 
-norm_env=function(data){
-  data$month=as.numeric(format(data$Date,"%m"))
-  data$CY=as.numeric(format(data$Date,"%Y"))
-  data$day=as.numeric(format(data$Date,"%d"))
+norm_env=function(stg.data){
+  stg.data$month=as.numeric(format(stg.data$Date,"%m"))
+  stg.data$CY=as.numeric(format(stg.data$Date,"%Y"))
+  stg.data$day=as.numeric(format(stg.data$Date,"%d"))
 
   norm.score=data.frame(norm.stage.score)
-  data=merge(data[,c("Date","month","day","CY","Data.Value")],
+  stg.data=merge(stg.data[,c("Date","month","day","CY","Data.Value")],
              norm.score,c("month","day"),all.x=T)
-  data=data[order(data$Date),]
+  stg.data=stg.data[order(stg.data$Date),]
 
-  leap=leap_year(data$Date)
-  yr=unique(data[leap==T,"CY"])
+  leap=leap_year(stg.data$Date)
+  yr=unique(stg.data[leap==T,"CY"])
   if(length(yr)>0){
   # Fills values for leap year
   for(i in 1:length(yr)){
   leap_dates=seq(as.Date(paste(yr[i],02,28,sep="-")),as.Date(paste(yr[i],03,01,sep="-")),"1 days")
-  data[data$CY==yr[i]&as.Date(data$Date)%in%leap_dates,6:17]=na.approx(data[data$CY==yr[i]&as.Date(data$Date)%in%leap_dates,6:17])
+  stg.data[stg.data$CY==yr[i]&as.Date(stg.data$Date)%in%leap_dates,6:17]=na.approx(stg.data[stg.data$CY==yr[i]&as.Date(stg.data$Date)%in%leap_dates,6:17])
   }
   }
   #Calculate Penalty
-  data$Pen_Highest=with(data,ifelse(Data.Value>=Up1_2,2+2*(Data.Value-Up1_2),NA))
-  data$Pen_Up1_2=with(data,ifelse(Data.Value>=Up05_1&Data.Value<Up1_2,1+(1/(Up1_2-Up05_1))*(Data.Value-Up05_1),NA))
-  data$Pen_Up05_1=with(data,ifelse(Data.Value>=Upper&Data.Value<Up05_1,0.5+(0.5/(Up05_1-Upper))*(Data.Value-Upper),NA))
-  data$Pen_Env=with(data,ifelse(Data.Value>=Low&Data.Value<Upper,0,NA))
-  data$Pen_Low3=with(data,ifelse(Data.Value<Low3,3+2*(Low3-Data.Value),NA))
-  data$Pen_Low05_1=with(data,ifelse(Data.Value<Low&Data.Value>=Low1_15,0.5+(0.5/(Low-Low1_15))*(Low-Data.Value),NA))
-  data$Pen_Low1_15=with(data,ifelse(Data.Value<Low1_15&Data.Value>=Low15_2,1+(0.5/(Low1_15-Low15_2))*(Low1_15-Data.Value),NA))
-  data$Pen_Low15_2=with(data,ifelse(Data.Value<Low15_2&Data.Value>=Low2_25,1.5+Low15_2-Data.Value,NA))
-  data$Pen_Low2_25=with(data,ifelse(Data.Value<Low2_25&Data.Value>=Low25_3,2+(0.5/(Low2_25-Low25_3))*(Low2_25-Data.Value),NA))
-  data$Pen_Low25_3=with(data,ifelse(Data.Value<Low25_3&Data.Value>=Low3,2.5+Low25_3-Data.Value,NA))
-  data$Pen_Low2_3=with(data,ifelse(Data.Value<Low2_3&Data.Value>=Low3,2+(2*(Low2_3-Data.Value)),NA))
-  data$score=apply(data[, 18:28], 1,FUN=function(x) max(x,na.rm=T))
+  stg.data$Pen_Highest=with(stg.data,ifelse(Data.Value>=Up1_2,2+2*(Data.Value-Up1_2),NA))
+  stg.data$Pen_Up1_2=with(stg.data,ifelse(Data.Value>=Up05_1&Data.Value<Up1_2,1+(1/(Up1_2-Up05_1))*(Data.Value-Up05_1),NA))
+  stg.data$Pen_Up05_1=with(stg.data,ifelse(Data.Value>=Upper&Data.Value<Up05_1,0.5+(0.5/(Up05_1-Upper))*(Data.Value-Upper),NA))
+  stg.data$Pen_Env=with(stg.data,ifelse(Data.Value>=Low&Data.Value<Upper,0,NA))
+  stg.data$Pen_Low3=with(stg.data,ifelse(Data.Value<Low3,3+2*(Low3-Data.Value),NA))
+  stg.data$Pen_Low05_1=with(stg.data,ifelse(Data.Value<Low&Data.Value>=Low1_15,0.5+(0.5/(Low-Low1_15))*(Low-Data.Value),NA))
+  stg.data$Pen_Low1_15=with(stg.data,ifelse(Data.Value<Low1_15&Data.Value>=Low15_2,1+(0.5/(Low1_15-Low15_2))*(Low1_15-Data.Value),NA))
+  stg.data$Pen_Low15_2=with(stg.data,ifelse(Data.Value<Low15_2&Data.Value>=Low2_25,1.5+Low15_2-Data.Value,NA))
+  stg.data$Pen_Low2_25=with(stg.data,ifelse(Data.Value<Low2_25&Data.Value>=Low25_3,2+(0.5/(Low2_25-Low25_3))*(Low2_25-Data.Value),NA))
+  stg.data$Pen_Low25_3=with(stg.data,ifelse(Data.Value<Low25_3&Data.Value>=Low3,2.5+Low25_3-Data.Value,NA))
+  stg.data$Pen_Low2_3=with(stg.data,ifelse(Data.Value<Low2_3&Data.Value>=Low3,2+(2*(Low2_3-Data.Value)),NA))
+  stg.data$score=apply(stg.data[, 18:28], 1,FUN=function(x) max(x,na.rm=T))
 
-  rslt=data[,c("Date","Data.Value","score")]
+  rslt=stg.data[,c("Date","Data.Value","score")]
   options(warn = -1)
   return(rslt)
 }
--- cran-comments.md
+++ cran-comments.md
@@ -5,4 +5,13 @@
 ## R CMD check results (2021-03-19)
 
 ### devtools::check() locally:
-0 errors √ | 0 warnings √ | 0 notes √
+```
+> checking R code for possible problems ... NOTE
+  norm_env: no visible binding for global variable 'norm.stage.score'
+  rec_env: no visible binding for global variable 'rec.stage.score'
+  Undefined global functions or variables:
+    norm.stage.score rec.stage.score
+```
+
+<span style="color:green">0 errors √</span> | <span style="color:green">0 warnings √</span> | <span style="color:red">1 note x</span>
+
